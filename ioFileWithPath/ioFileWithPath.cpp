b84601d4f29836eb6067e276c85ef3f2d593ce58
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-06-21 11:17:33 +0700

    menambahkan namafile txt di infile

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -33,7 +33,8 @@ int main() {
 	//membuka file dalam mode membaca 
 	ifstream infile;
 	//menunjuk ke sebuah file 
-	infile.open(NamaFile, ios::in);
+	infile.open(NamaFile+ ".txt", ios::in);
+
 	cout << endl << ">= Membuka dan Membaca file" << endl;
 	//jika file ada maka 
 	if (infile.is_open())
