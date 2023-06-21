36edf7e63a857e9a47fb3107b1fb15bc22f0188a
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-06-21 11:08:33 +0700

    jika file ada maka akan melakukan perulangan

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -35,5 +35,7 @@ int main() {
 	//menunjuk ke sebuah file 
 	infile.open(NamaFile, ios::in);
 	cout << endl << ">= Membuka dan Membaca file" << endl;
-	
-	
+	//jika file ada maka 
+	if (infile.is_open())
+	{
+		
