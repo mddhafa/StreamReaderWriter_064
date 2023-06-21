4e84a1d255ca606bfb744798ae05bbc708c406cf
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-06-21 11:10:52 +0700

    menambahkan perulangan dan return

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -38,4 +38,16 @@ int main() {
 	//jika file ada maka 
 	if (infile.is_open())
 	{
-		
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini 
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai 
+		infile.close();
+	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
+}
