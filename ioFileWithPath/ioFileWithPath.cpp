bb7e88ae92d4323361b87943379614145c8691e7
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-06-21 11:04:32 +0700

    menambahkan outfile danwhile

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -9,3 +9,25 @@ int main() {
 
 	cout << "Masukkan Nama File :";
 	cin >> NamaFile;
+
+	// membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << "<= Menulis File, \'q\' untuk keluar" << endl;
+
+	// unlimitade loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan q
+		if (baris == "q") break;
+		// menulis dan memasukkan nilai dari 'baris' ke dalam file 
+		outfile << baris << endl;
+	}
+	//selesai damam menulis sekarang tutup filenya
+	outfile.close();
+
+	
