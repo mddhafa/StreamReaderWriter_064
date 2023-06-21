e193635fccce4c5a36ca95d250cb8b01d9e525fd
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-06-21 10:33:30 +0700

    menambahkan while

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -13,3 +13,14 @@ int main() {
 
 	cout << "<= Menulis File, \'q\' untuk keluar" << endl;
 	 
+	// unlimitade loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan q
+		if (baris == "q") break;
+		// menulis dan memasukkan nilai dari 'baris' ke dalam file 
+		outfile << baris << endl;
+	}
+}
